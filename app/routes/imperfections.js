ef72cbf91d49f2cb0d7f0bd8208d1dffc9be925c
princejonn <jonn@jonn.nu>
2017-08-15 16:11:38 +0200

    imperfections

---
+++ app/routes/imperfections.js
@@ -0,0 +1,99 @@
+var comment = require('./../../lib/sql/comment'),
+    ownership = require('./../../lib/sql/ownership'),
+    relation = require('./../../lib/sql/relation'),
+    sequel = require('./../../lib/sql/sequel');
+
+module.exports = function(router) {
+    'use strict';
+
+    var tableName = 'imperfection',
+        userContent = true,
+        adminRestriction = false,
+        useUpdateColumn = true;
+
+    var sql = 'SELECT * FROM imperfection';
+
+    router.route('/')
+        .get(function(req, res, next) {
+            var call = sql + ' WHERE ' +
+                'imperfection.canon = 1 AND ' +
+                'imperfection.manifestation_id IS NULL AND ' +
+                'imperfection.species_id IS NULL AND ' +
+                'imperfection.deleted IS NULL';
+
+            sequel.get(req, res, next, call);
+        })
+        .post(function(req, res, next) {
+            sequel.post(req, res, next, tableName, adminRestriction, userContent);
+        });
+
+    // Manifestations
+
+    router.route('/manifestation/:manifestationId')
+        .get(function(req, res, next) {
+            var call = sql + ' WHERE ' +
+                'imperfection.canon = 1 AND ' +
+                'imperfection.manifestation_id = ? AND ' +
+                'imperfection.species_id IS NULL AND ' +
+                'imperfection.deleted IS NULL';
+
+            sequel.get(req, res, next, call, [req.params.manifestationId]);
+        });
+
+    // Species
+
+    router.route('/species/:speciesId')
+        .get(function(req, res, next) {
+            var call = sql + ' WHERE ' +
+                'imperfection.canon = 1 AND ' +
+                'imperfection.manifestation_id IS NULL AND ' +
+                'imperfection.species_id = ? AND ' +
+                'imperfection.deleted IS NULL';
+
+            sequel.get(req, res, next, call, [req.params.speciesId]);
+        });
+
+    // ID
+
+    router.route('/:imperfectionId')
+        .get(function(req, res, next) {
+            var call = sql + ' WHERE imperfection.id = ? AND imperfection.deleted IS NULL';
+
+            sequel.get(req, res, next, call, [req.params.imperfectionId], true);
+        })
+        .put(function(req, res, next) {
+            sequel.put(req, res, next, tableName, req.params.imperfectionId, adminRestriction, useUpdateColumn);
+        })
+        .delete(function(req, res, next) {
+            sequel.delete(req, res, next, tableName, req.params.imperfectionId, adminRestriction);
+        });
+
+    router.route('/:imperfectionId/canon')
+        .put(function(req, res, next) {
+            sequel.canon(req, res, next, tableName, req.params.imperfectionId, useUpdateColumn);
+        });
+
+    router.route('/:imperfectionId/clone')
+        .post(function(req, res, next) {
+            sequel.clone(req, res, next, tableName, req.params.imperfectionId, adminRestriction, userContent);
+        });
+
+    router.route('/:imperfectionId/comments')
+        .get(function(req, res, next) {
+            comment.get(req, res, next, tableName, req.params.imperfectionId);
+        })
+        .post(function(req, res, next) {
+            comment.post(req, res, next, tableName, req.params.imperfectionId);
+        });
+
+    router.route('/:imperfectionId/ownership')
+        .get(function(req, res) {
+            ownership(req, tableName, req.params.imperfectionId, adminRestriction, function(err) {
+                var ownership = true;
+
+                if(err) ownership = false;
+
+                res.status(200).send({success: true, message: 'Ownership verified', ownership: ownership});
+            })
+        });
+};
---
+++ test/routes/imperfections.js
@@ -0,0 +1,252 @@
+var async = require('async'),
+    _ = require('underscore'),
+    chai = require('chai'),
+    validator = require('validator');
+
+var should = chai.should(),
+    assert = chai.assert,
+    expect = chai.expect;
+
+var app = require('./../app'),
+    hasher = require('./../../lib/hasher');
+
+describe('/imperfections', function() {
+
+    before(function(done) {
+        app.login(done);
+    });
+
+    var temporaryId;
+
+    function verifyList(body) {
+        assert.isNumber(body.length);
+
+        assert.isArray(body.results);
+        assert.lengthOf(body.results, body.length);
+
+        if(body.length > 0) {
+            _.each(body.results, function(item) {
+                verifyItem(item);
+            });
+        }
+
+        assert.isObject(body.fields);
+    }
+
+    function verifyItem(item) {
+        assert.isNumber(item.id);
+        assert.isBoolean(item.canon);
+        assert.isNumber(item.popularity);
+
+        assert.isString(item.name);
+        if(item.description) assert.isString(item.description);
+        if(item.manifestation_id) assert.isNumber(item.manifestation_id);
+        if(item.species_id) assert.isNumber(item.species_id);
+
+        assert.isString(item.created);
+        if(item.updated) assert.isString(item.updated);
+        if(item.deleted) assert.isString(item.deleted);
+    }
+
+
+    describe('POST', function() {
+
+        it('POST / should create a new asset', function(done) {
+            var payload = {
+                name: hasher(20),
+                description: hasher(20),
+                manifestation_id: 1,
+                species_id: 1
+            };
+
+            app.post('/imperfections', payload)
+                .expect(201)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    assert.isNumber(res.body.affected);
+                    assert.notEqual(res.body.affected, 0);
+
+                    assert.isNumber(res.body.id);
+
+                    temporaryId = res.body.id;
+
+                    done();
+                });
+        });
+
+        it('POST /:imperfectionId/clone should create a copy of the asset', function(done) {
+            app.post('/imperfections/' + temporaryId + '/clone')
+                .expect(201)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    assert.isNumber(res.body.affected);
+                    assert.isNumber(res.body.id);
+
+                    done();
+                });
+        });
+
+        it('POST /:imperfectionId/comments should create a new comment for the asset', function(done) {
+            var payload = {
+                content: hasher(20)
+            };
+
+            app.post('/imperfections/' + temporaryId + '/comments', payload)
+                .expect(201)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    assert.isNumber(res.body.affected);
+                    assert.isNumber(res.body.id);
+
+                    done();
+                });
+        });
+
+    });
+
+    describe('PUT', function() {
+
+        it('PUT /:imperfectionId should update the item with new values', function(done) {
+            var payload = {
+                name: hasher(20),
+                description: hasher(20)
+            };
+
+            app.put('/imperfections/' + temporaryId, payload)
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    assert.isNumber(res.body.changed);
+                    assert.notEqual(res.body.changed, 0);
+
+                    assert.isNumber(res.body.id);
+
+                    done();
+                })
+        });
+
+        it('PUT /:imperfectionId/canon should update the asset canon field', function(done) {
+            app.put('/imperfections/' + temporaryId + '/canon')
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    assert.isNumber(res.body.changed);
+                    assert.notEqual(res.body.changed, 0);
+
+                    assert.isNumber(res.body.id);
+
+                    done();
+                });
+        });
+
+    });
+
+    describe('GET', function() {
+
+        it('GET / should return a list of imperfections', function(done) {
+            app.get('/imperfections')
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    verifyList(res.body);
+
+                    done();
+                });
+        });
+
+        it('GET /manifestation/:manifestationId should return a list of imperfections', function(done) {
+            app.get('/imperfections/manifestation/1')
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    verifyList(res.body);
+
+                    done();
+                });
+        });
+
+        it('GET /species/:speciesId should return a list of imperfections', function(done) {
+            app.get('/imperfections/species/1')
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    verifyList(res.body);
+
+                    done();
+                });
+        });
+
+        it('GET /:imperfectionId should return one asset', function(done) {
+            app.get('/imperfections/' + temporaryId)
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    verifyItem(res.body.result);
+
+                    done();
+                })
+        });
+
+        it('GET /:imperfectionId/ownership should return ownership status of the asset if user is logged in', function(done) {
+            app.get('/imperfections/' + temporaryId + '/ownership')
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    assert.isBoolean(res.body.ownership);
+
+                    done();
+                });
+        });
+
+        it('GET /:imperfectionId/comments should get all available comments for the asset', function(done) {
+            app.get('/imperfections/' + temporaryId + '/comments')
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    _.each(res.body.results, function(comment) {
+                        assert.isNumber(comment.id);
+                        assert.isString(comment.content);
+
+                        assert.isNumber(comment.user_id);
+                        assert.isString(comment.displayname);
+
+                        assert.isString(comment.created);
+                        if(comment.updated) assert.isString(comment.updated);
+                        assert.isNull(comment.deleted);
+                    });
+
+                    done();
+                })
+        });
+
+    });
+
+    describe('DELETE', function() {
+
+        it('DELETE /:imperfectionId should update the asset deleted field', function(done) {
+            app.delete('/imperfections/' + temporaryId)
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    assert.isNumber(res.body.affected);
+                    assert.notEqual(res.body.affected, 0);
+
+                    done();
+                })
+        });
+
+    });
+
+});
