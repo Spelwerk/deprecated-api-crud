3440511aaef4fef3307faf0a9d5457f227e1990c
Jonn Nilsson <jonn@jonn.nu>
2017-10-02 13:06:07 +0200

    refactoring

---
+++ app/routes/locations.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var generic = require('../../lib/helper/generic'),
+    locations = require('../../lib/tables/locations');
+
+module.exports = function(router) {
+    var tableName = 'country';
+
+    var sql = 'SELECT * FROM ' + tableName + ' ' +
+        'LEFT JOIN ' + tableName + '_is_copy ON ' + tableName + '_is_copy.' + tableName + '_id = ' + tableName + '.id';
+
+    generic.root(router, tableName, sql);
+
+    router.route('/')
+        .post(function(req, res, next) {
+            var name = req.body.name,
+                description = req.body.description,
+                price = req.body.price,
+                countryId = req.body.country_id,
+                individualId = req.body.individual_id,
+                locationId = req.body.location_id;
+
+            locations.post(req.user, name, description, price, countryId, individualId, locationId, function(err, id) {
+                if(err) return next(err);
+
+                res.status(201).send({id: id});
+            });
+        });
+
+    generic.deleted(router, tableName, sql);
+
+    // ID
+
+    generic.get(router, tableName, sql);
+    generic.put(router, tableName, false, true);
+    generic.delete(router, tableName, false, true);
+    generic.canon(router, tableName);
+    generic.clone(router, tableName);
+    generic.comments(router, tableName);
+    generic.labels(router, tableName);
+    generic.ownership(router, tableName);
+    generic.revive(router, tableName);
+};
---
+++ test/routes/locations.js
@@ -0,0 +1,218 @@
+var async = require('async'),
+    _ = require('underscore'),
+    chai = require('chai'),
+    validator = require('validator');
+
+var should = chai.should(),
+    assert = chai.assert,
+    expect = chai.expect;
+
+var app = require('../app'),
+    verifier = require('./../verifier'),
+    hasher = require('../../lib/hasher');
+
+describe('/locations', function() {
+
+    var baseRoute = '/locations';
+
+    var temporaryId;
+
+    before(function(done) {
+        app.login(done);
+    });
+
+    var countryId;
+    before(function(done) {
+        app.get('/countries')
+            .expect(200)
+            .end(function(err, res) {
+                if(err) return done(err);
+
+                countryId = res.body.results[0].id;
+
+                done();
+            });
+    });
+
+    var individualId;
+    before(function(done) {
+        app.get('/individuals')
+            .expect(200)
+            .end(function(err, res) {
+                if(err) return done(err);
+
+                individualId = res.body.results[0].id;
+
+                done();
+            });
+    });
+
+    var locationId;
+    before(function(done) {
+        app.get('/locations')
+            .expect(200)
+            .end(function(err, res) {
+                if(err) return done(err);
+
+                locationId = res.body.results[0].id;
+
+                done();
+            });
+    });
+
+    function verifyList(body) {
+        assert.isNumber(body.length);
+
+        assert.isArray(body.results);
+        assert.lengthOf(body.results, body.length);
+
+        if(body.length > 0) {
+            _.each(body.results, function(item) {
+                verifyItem(item);
+            });
+        }
+
+        assert.isObject(body.fields);
+    }
+
+    function verifyItem(item) {
+        verifier.generic(item);
+
+        if(item.country_id) assert.isNumber(item.country_id);
+        if(item.individual_id) assert.isNumber(item.individual_id);
+        if(item.location_id) assert.isNumber(item.location_id);
+    }
+
+
+    describe('POST', function() {
+
+        it('/ should create a new item', function(done) {
+            var payload = {
+                name: hasher(20),
+                description: hasher(20),
+                country_id: countryId,
+                individual_id: individualId,
+                location_id: locationId
+            };
+
+            app.post(baseRoute, payload)
+                .expect(201)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    assert.isNumber(res.body.id);
+
+                    temporaryId = res.body.id;
+
+                    done();
+                });
+        });
+
+        it('/:id/clone should create a copy', function(done) {
+            app.post(baseRoute + '/' + temporaryId + '/clone')
+                .expect(201)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    assert.isNumber(res.body.id);
+
+                    done();
+                });
+        });
+
+        it('/:id/comments should create a new comment', function(done) {
+            app.post(baseRoute + '/' + temporaryId + '/comments', { comment: hasher(20) })
+                .expect(201)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    assert.isNumber(res.body.id);
+
+                    done();
+                });
+        });
+
+        it('/:id/labels should create a label', function(done) {
+            app.post(baseRoute + '/' + temporaryId + '/labels', { label: hasher(20) })
+                .expect(201)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    assert.isNumber(res.body.id);
+
+                    done();
+                })
+        });
+
+    });
+
+    describe('PUT', function() {
+
+        it('/:id should update the item with new values', function(done) {
+            var payload = {
+                name: hasher(20),
+                description: hasher(20)
+            };
+
+            app.put(baseRoute + '/' + temporaryId, payload)
+                .expect(204)
+                .end(done);
+        });
+
+        it('/:id/canon/:canon should update the canon status', function(done) {
+            app.put(baseRoute + '/' + temporaryId + '/canon/1')
+                .expect(204)
+                .end(done);
+        });
+
+    });
+
+    describe('GET', function() {
+
+        it('/ should return a list', function(done) {
+            app.get(baseRoute)
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    verifyList(res.body);
+
+                    done();
+                });
+        });
+
+        it('/deleted should return a list of deleted items', function(done) {
+            app.get(baseRoute + '/deleted')
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    verifyList(res.body);
+
+                    done();
+                });
+        });
+
+        it('/:id should return one item', function(done) {
+            app.get(baseRoute + '/' + temporaryId)
+                .expect(200)
+                .end(function(err, res) {
+                    if(err) return done(err);
+
+                    verifyItem(res.body.result);
+
+                    done();
+                })
+        });
+
+        it('/:id/ownership should return ownership status', function(done) {
+            app.get(baseRoute + '/' + temporaryId + '/ownership').expect(200).end(function(err, res) { verifier.ownership(err, res, done); });
+        });
+
+        it('/:id/comments should get all available comments', function(done) {
+            app.get(baseRoute + '/' + temporaryId + '/comments').expect(200).end(function(err, res) { verifier.comments(err, res, done); });
+        });
+
+    });
+
+});
