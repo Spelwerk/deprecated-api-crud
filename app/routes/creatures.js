df0b0274210db070142e16772b89bae26f32291f
Jonn Nilsson <jonn@jonn.nu>
2017-10-03 15:37:57 +0200

    refactoring

--- app/routes/creatures.js
+++ app/routes/creatures.js
@@ -6,18 +6,41 @@ var generic = require('../../lib/helper/generic'),
 module.exports = function(router) {
     var tableName = 'creature';
 
-    var sql = 'SELECT * FROM ' + tableName;
+    var sql = 'SELECT * FROM creature ' +
+
+        'LEFT JOIN creature_appearance ON creature_appearance.creature_id = creature.id ' +
+        'LEFT JOIN creature_age ON creature_age.creature_id = creature.id ' +
+        'LEFT JOIN creature_biography ON creature_biography.creature_id = creature.id ' +
+        'LEFT JOIN creature_description ON creature_description.creature_id = creature.id ' +
+        'LEFT JOIN creature_drive ON creature_drive.creature_id = creature.id ' +
+        'LEFT JOIN creature_gender ON creature_gender.creature_id = creature.id ' +
+        'LEFT JOIN creature_occupation ON creature_occupation.creature_id = creature.id ' +
+        'LEFT JOIN creature_personality ON creature_personality.creature_id = creature.id ' +
+        'LEFT JOIN creature_points ON creature_points.creature_id = creature.id ' +
+        'LEFT JOIN creature_pride ON creature_pride.creature_id = creature.id ' +
+        'LEFT JOIN creature_problem ON creature_problem.creature_id = creature.id ' +
+        'LEFT JOIN creature_shame ON creature_shame.creature_id = creature.id ' +
+
+        'LEFT JOIN creature_is_copy ON creature_is_copy.creature_id = creature.id ' +
+        'LEFT JOIN creature_is_corporation ON creature_is_corporation.creature_id = creature.id ' +
+        'LEFT JOIN creature_is_country ON creature_is_country.creature_id = creature.id ' +
+        'LEFT JOIN creature_is_identity ON creature_is_identity.creature_id = creature.id ' +
+        'LEFT JOIN creature_is_nature ON creature_is_nature.creature_id = creature.id ' +
+        'LEFT JOIN creature_is_wealth ON creature_is_wealth.creature_id = creature.id';
 
     generic.root(router, tableName, sql);
 
     router.route('/')
         .post(function(req, res, next) {
-            var name = req.body.name,
-                description = req.body.description,
+            var firstName = req.body.firstname,
+                nickName = req.body.nickname,
+                middleName = req.body.middlename,
+                lastName = req.body.lastname,
                 worldId = req.body.world_id,
-                speciesId = req.body.species_id;
+                speciesId = req.body.species_id,
+                description = req.body.description;
 
-            creatures.post(req.user, name, description, worldId, speciesId, function(err, id) {
+            creatures.post(req.user, firstName, nickName, middleName, lastName, worldId, speciesId, description, function(err, id) {
                 if(err) return next(err);
 
                 res.status(201).send({id: id});
@@ -26,7 +49,49 @@ module.exports = function(router) {
 
     generic.deleted(router, tableName, sql);
     generic.get(router, tableName, sql);
-    generic.put(router, tableName, false, true);
+
+    router.route('/:id')
+        .put(function(req, res, next) {
+            var id = req.params.id,
+                firstName = req.body.firstname,
+                nickName = req.body.nickname,
+                middleName = req.body.middlename,
+                lastName = req.body.lastname,
+                calculated = req.body.calculated,
+                manifestation = req.body.manifestation;
+
+            var pointDoctrine = req.body.point_doctrine,
+                pointExpertise = req.body.point_expertise,
+                pointGift = req.body.point_gift,
+                pointImperfection = req.body.point_imperfection,
+                pointMilestone = req.body.point_milestone,
+                pointSkill = req.body.point_skill;
+
+            var appearance = req.body.appearance,
+                age = req.body.age,
+                biography = req.body.biography,
+                description = req.body.description,
+                drive = req.body.drive,
+                gender = req.body.gender,
+                occupation = req.body.occupation,
+                personality = req.body.personality,
+                pride = req.body.pride,
+                problem = req.body.problem,
+                shame = req.body.shame;
+
+            var corporationId = req.body.corporation_id,
+                countryId = req.body.country_id,
+                identityId = req.body.identity_id,
+                natureId = req.body.nature_id,
+                wealthId = req.body.wealth_id;
+
+            creatures.put(req.user, id, firstName, nickName, middleName, lastName, calculated, manifestation, appearance, age, biography, description, drive, gender, occupation, personality, pride, problem, shame, pointDoctrine, pointExpertise, pointGift, pointImperfection, pointMilestone, pointSkill, corporationId, countryId, identityId, natureId, wealthId, function(err) {
+                if(err) return next(err);
+
+                res.status(204).send();
+            });
+        });
+
     generic.delete(router, tableName, false, true);
     generic.canon(router, tableName);
     generic.clone(router, tableName);
--- test/routes/creatures.js
+++ test/routes/creatures.js
@@ -21,19 +21,6 @@ describe('/creatures', function() {
         app.login(done);
     });
 
-    var attributeId;
-    before(function(done) {
-        app.get('/attributes')
-            .expect(200)
-            .end(function(err, res) {
-                if(err) return done(err);
-
-                attributeId = res.body.results[0].id;
-
-                done();
-            });
-    });
-
     var speciesId;
     before(function(done) {
         app.get('/species')
@@ -85,37 +72,17 @@ describe('/creatures', function() {
     }
 
 
-    it('/ should create a new item', function(done) {
-        var payload = {
-            name: hasher(20),
-            description: hasher(20),
-            world_id: worldId,
-            species_id: speciesId
-        };
-
-        app.post(baseRoute, payload)
-            .expect(201)
-            .end(function(err, res) {
-                if(err) return done(err);
-
-                assert.isNumber(res.body.id);
-
-                temporaryId = res.body.id;
-
-                done();
-            });
-    });
-
-    xdescribe('POST', function() {
+    describe('POST', function() {
 
         it('/ should create a new item', function(done) {
             var payload = {
-                name: hasher(20),
-                description: hasher(20),
-                assettype_id: speciesId,
-                equipable: true,
-                legal: true,
-                price: 10
+                firstname: hasher(20),
+                nickname: hasher(20),
+                middlename: hasher(20),
+                lastname: hasher(20),
+                world_id: worldId,
+                species_id: speciesId,
+                description: hasher(40)
             };
 
             app.post(baseRoute, payload)
@@ -167,47 +134,35 @@ describe('/creatures', function() {
                 })
         });
 
-        it('/:id/attributes should add a relation to the item', function(done) {
-            var payload = {
-                insert_id: 1,
-                value: 10
-            };
-
-            app.post(baseRoute + '/' + temporaryId + '/attributes', payload)
-                .expect(201)
-                .end(done);
-        });
-
-        it('/:id/doctrines should add a relation to the item', function(done) {
-            var payload = {
-                insert_id: 1,
-                value: 10
-            };
-
-            app.post(baseRoute + '/' + temporaryId + '/doctrines', payload)
-                .expect(201)
-                .end(done);
-        });
-
-        it('/:id/skills should add a relation to the item', function(done) {
-            var payload = {
-                insert_id: 1,
-                value: 10
-            };
-
-            app.post(baseRoute + '/' + temporaryId + '/skills', payload)
-                .expect(201)
-                .end(done);
-        });
-
     });
 
-    xdescribe('PUT', function() {
+    describe('PUT', function() {
 
         it('/:id should update the item with new values', function(done) {
             var payload = {
-                name: hasher(20),
-                description: hasher(20)
+                firstname: hasher(20),
+                nickname: hasher(20),
+                middlename: hasher(20),
+                lastname: hasher(20),
+                calculated: true,
+                manifestation: true,
+                appearance: hasher(20),
+                age: 50,
+                biography: hasher(20),
+                description: hasher(20),
+                drive: hasher(20),
+                gender: hasher(20),
+                occupation: hasher(20),
+                personality: hasher(20),
+                pride: hasher(20),
+                problem: hasher(20),
+                shame: hasher(20),
+                point_doctrine: 1,
+                point_expertise: 2,
+                point_gift: 3,
+                point_imperfection: 4,
+                point_milestone: 5,
+                point_skill: 6
             };
 
             app.put(baseRoute + '/' + temporaryId, payload)
@@ -217,24 +172,6 @@ describe('/creatures', function() {
 
         it('/:id/canon/:canon should update the canon status', function(done) {
             app.put(baseRoute + '/' + temporaryId + '/canon/1')
-                .expect(204)
-                .end(done);
-        });
-
-        it('/:id/attributes should change the value', function(done) {
-            app.put(baseRoute + '/' + temporaryId + '/attributes/' + attributeId, {value: 8})
-                .expect(204)
-                .end(done);
-        });
-
-        it('/:id/doctrines should change the value', function(done) {
-            app.put(baseRoute + '/' + temporaryId + '/doctrines/' + doctrineId, {value: 8})
-                .expect(204)
-                .end(done);
-        });
-
-        it('/:id/skills should change the value', function(done) {
-            app.put(baseRoute + '/' + temporaryId + '/skills/' + skillId, {value: 8})
                 .expect(204)
                 .end(done);
         });
@@ -297,57 +234,6 @@ describe('/creatures', function() {
 
         it('/:id/comments should get all available comments', function(done) {
             app.get(baseRoute + '/' + temporaryId + '/comments').expect(200).end(function(err, res) { verifier.comments(err, res, done); });
-        });
-
-        it('/:id/attributes should return a list', function(done) {
-            app.get(baseRoute + '/' + temporaryId + '/attributes')
-                .expect(200)
-                .end(function(err, res) {
-                    if(err) return done(err);
-
-                    assert.isNumber(res.body.length);
-                    assert.isArray(res.body.results);
-
-                    _.each(res.body.results, function(item) {
-                        verifier.generic(item);
-                    });
-
-                    done();
-                });
-        });
-
-        it('/:id/doctrines should return a list', function(done) {
-            app.get(baseRoute + '/' + temporaryId + '/doctrines')
-                .expect(200)
-                .end(function(err, res) {
-                    if(err) return done(err);
-
-                    assert.isNumber(res.body.length);
-                    assert.isArray(res.body.results);
-
-                    _.each(res.body.results, function(item) {
-                        verifier.generic(item);
-                    });
-
-                    done();
-                });
-        });
-
-        it('/:id/skills should return a list', function(done) {
-            app.get(baseRoute + '/' + temporaryId + '/skills')
-                .expect(200)
-                .end(function(err, res) {
-                    if(err) return done(err);
-
-                    assert.isNumber(res.body.length);
-                    assert.isArray(res.body.results);
-
-                    _.each(res.body.results, function(item) {
-                        verifier.generic(item);
-                    });
-
-                    done();
-                });
         });
 
     });
