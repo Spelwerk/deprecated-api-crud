9d63901f9ba0c58031891c7dc97a360a69a0d6db
Jonn Nilsson <jonn@jonn.nu>
2017-10-10 10:57:25 +0200

    refactoring

--- app/initializers/errors.js
+++ app/initializers/errors.js
@@ -5,18 +5,12 @@ module.exports = function(app, callback) {
 
     // Return error information as response
     app.use(function(err, req, res, next) {
-        err.status = err.status || 500;
-        err.title = err.title || 'Error';
-        err.message = err.message || 'The server encountered an error';
-        err.details = err.details || null;
-
         req.log.error = err;
-
         logger.error(req.log);
 
         if(environment !== 'production') console.error(req.log);
 
-        res.status(err.status).send({id: req.log.id, title: err.title, message: err.message, details: err.details});
+        res.status(err.getStatus()).send(err.getSend());
     });
 
     callback();
