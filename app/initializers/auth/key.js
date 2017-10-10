7586e44029d2403cbc640fcdfc55f4e5597857a0
Jonn Nilsson <jonn@jonn.nu>
2017-10-10 15:16:59 +0200

    refactoring

--- app/initializers/auth/key.js
+++ app/initializers/auth/key.js
@@ -1,16 +1,20 @@
 'use strict';
 
+let AppError = require('../../../lib/errors/app-error');
+
 var nconf = require('nconf'),
     basicAuth = require('basic-auth');
 
 module.exports = function(app, callback) {
-    var apiKeyId = nconf.get('api-key:id'),
+    let apiKeyId = nconf.get('api-key:id'),
         apiKeySecret = nconf.get('api-key:secret');
 
     app.use(function(req, res, next) {
-        var credentials = basicAuth(req);
+        let credentials = basicAuth(req);
 
-        if(!credentials || credentials.name !== apiKeyId || credentials.pass !== apiKeySecret) return next({status: 403, message: 'Faulty API credentials', error: 'The credentials in basic auth were not correct.'});
+        if(!credentials) return next(new AppError(401, "Missing Credentials", "Credentials are missing in the header"));
+
+        if(credentials.name !== apiKeyId || credentials.pass !== apiKeySecret) return next(new AppError(403, "Faulty API Credentials", "The API key provided was not correct."));
 
         next();
     });
--- app/initializers/errors.js
+++ app/initializers/errors.js
@@ -1,16 +1,17 @@
 'use strict';
 
-module.exports = function(app, callback) {
-    var logger = require(appRoot + '/lib/logger');
+var logger = require(appRoot + '/lib/logger');
 
+module.exports = function(app, callback) {
     // Return error information as response
     app.use(function(err, req, res, next) {
         req.log.error = err;
+
         logger.error(req.log);
 
         if(environment !== 'production') console.error(req.log);
 
-        res.status(err.getStatus()).send(err.getSend());
+        res.status(err.status).send({title: err.title, message: err.message});
     });
 
     callback();
