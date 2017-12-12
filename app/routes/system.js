0b385c3f7d5f87169fd7ab356c0dca7e9660ace3
Jonn Nilsson <jonn@jonn.nu>
2017-12-12 11:25:38 +0100

    refactoring

--- app/routes/system.js
+++ app/routes/system.js
@@ -2,7 +2,10 @@
 
 let yaml = require('node-yaml').readSync;
 
-let defaults = yaml('./../../config/defaults.yml');
+let defaults = yaml('./../../config/defaults.yml'),
+    dice = yaml('./../../config/dice.yml'),
+    icons = yaml('./../../config/icons.yml'),
+    points = yaml('./../../config/points.yml');
 
 module.exports = function(router) {
 
@@ -11,9 +14,24 @@ module.exports = function(router) {
             res.status(204).send();
         });
 
-    router.route('/defaults')
+    router.route('/config/defaults')
         .get(function(req, res) {
             res.status(200).send(defaults);
+        });
+
+    router.route('/config/dice')
+        .get(function(req, res) {
+            res.status(200).send(dice);
+        });
+
+    router.route('/config/icons')
+        .get(function(req, res) {
+            res.status(200).send(icons);
+        });
+
+    router.route('/config/points')
+        .get(function(req, res) {
+            res.status(200).send(points);
         });
 
 };
